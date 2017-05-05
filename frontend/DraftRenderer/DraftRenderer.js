9bb58161753cd704b06124764e2b7d96fa0aa20a
fdemian <demian.fc@gmail.com>
2017-05-05 20:58:35 -0300

    Convert draft-renderer into a functional component and move its renderers to a different file.

--- frontend/DraftRenderer/DraftRenderer.js
+++ frontend/DraftRenderer/DraftRenderer.js
@@ -1,93 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import redraft from 'redraft';
-import Spoiler from '../Editor/TextElements/Spoiler/Spoiler';
-import Link from '../Editor/TextElements/Link/Link';
-import EmbededVideo from '../Editor/TextElements/Media/EmbededVideo';
-import QuoteBlock from '../Editor/TextElements/QuoteBlock/QuoteBlock';
+import renderers from './Renderers';
 
-/* Style callbacks */
-const styles = {
-  code: {
-    backgroundColor: 'rgba(0, 0, 0, 0.05)',
-    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
-    fontSize: 16,
-    padding: 2,
-  },
-  codeBlock: {
-    backgroundColor: 'rgba(0, 0, 0, 0.05)',
-    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
-    fontSize: 16,
-    padding: 20,
-  },
-  listItem: {
-    float: 'none'
+const RenderWarning = () => {
+    return (<div>Nothing to render.</div>);
+}
+
+const Renderer = ({raw}) => {
+
+  if (!raw)
+      return <RenderWarning />;
+
+  const rendered = redraft(raw, renderers);
+
+  if (!rendered) {
+      return <RenderWarning />;
   }
-};
 
-// Adds a <br /> after a block.
-const addBreaklines = (children) => children.map(child => [child, <br />]);
-
-const renderers = {
-
-  /**
-   * Those callbacks will be called recursively to render a nested structure
-   */
-  inline: {
-    BOLD: (children) => <strong>{children}</strong>,
-    ITALIC: (children) => <em>{children}</em>,
-    UNDERLINE: (children) => <u>{children}</u>,
-    CODE: (children) => <span style={styles.code}>{children}</span>,
-  },
-
-  /**
-   * Blocks receive children and depth
-   * Note that children are an array of blocks with same styling,
-   */
-  blocks: {
-     unstyled: (children) => children.map(child => <p>{child}</p>),
-     blockquote: (children) => <blockquote key={1}>{addBreaklines(children)}</blockquote>,
-    'header-one': (children) => children.map(child => <h1>{child}</h1>),
-    'header-two': (children) => children.map(child => <h2>{child}</h2>),
-    'code-block': (children) => <pre style={styles.codeBlock}>{addBreaklines(children)}</pre>,
-    'unordered-list-item': (children, depth) => <ul>{children.map(child => <li style={styles.listItem}>{child}</li>)}</ul>,
-    'ordered-list-item': (children, depth) => <ol>{children.map(child => <li style={styles.listItem}>{child}</li>)}</ol>,
-  },
-
-  /* Entities receive children and the entity data */
-  entities: {
-    Image: (children, data) => <div><img src={data.src} /></div>,
-	LINK: (children, data) =>  <a href={data.url} >{children}</a>,
-	SPOILER: (children, data) => <Spoiler text={children[0]} />,
-    Video: (children, data) => <div><EmbededVideo src={data.src} /></div>,
-    QuoteBlock: (children, data) => <QuoteBlock comment={data.props} />
-  },
+  return (<div>{rendered}</div>);
 
 }
 
-export default class Renderer extends Component {
-
-  renderWarning() {
-    return <div>Nothing to render.</div>;
-  }
-
-  render() {
-
-    const { raw } = this.props;
-
-    if (!raw) {
-      return this.renderWarning();
-    }
-
-    const rendered = redraft(raw, renderers);
-
-    // redraft returns a null if there's nothing to render
-    if (!rendered) {
-      return this.renderWarning();
-    }
-    return (
-      <div>
-        {rendered}
-      </div>
-    );
-  }
-}
+export default Renderer;
